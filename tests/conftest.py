3ec32f9f721f736777c2e8ccda36659cd5efe96d
Zachary Levesque <zlevesque93@gmail.com>
2026-05-07 19:11:51 -0400

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+
+
+ROOT = Path(__file__).resolve().parent.parent
+PYTHON_DIR = ROOT / "python"
+
+if str(PYTHON_DIR) not in sys.path:
+    sys.path.insert(0, str(PYTHON_DIR))
